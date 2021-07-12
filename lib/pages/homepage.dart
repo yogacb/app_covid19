66823931f92a674f66509ccd46e0aff4ef5de3c6
yogacb <pamungkasyoga17@gmail.com>
2021-07-12 23:36:49 +0700

    add some code

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(top: 35, left: 21),
+          padding: const EdgeInsets.only(top: 35, left: 21, right: 21),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -25,10 +25,63 @@ class HomePage extends StatelessWidget {
                   fontWeight: FontWeight.w300,
                 ),
               ),
-              Container(
-                height: 92,
-                width: 159,
-                color: redColor,
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 92,
+                    width: 159,
+                    color: redColor,
+                  ),
+                  Container(
+                    height: 92,
+                    width: 159,
+                    color: blueColor,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 18,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 92,
+                    width: 159,
+                    decoration: BoxDecoration(
+                      color: purpleColor,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: 0,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'Meninggal',
+                            style: whiteTextStyle.copyWith(fontSize: 24),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            '2.000.000',
+                            style: whiteTextStyle.copyWith(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               )
             ],
           ),
--- lib/theme.dart
+++ lib/theme.dart
@@ -7,7 +7,12 @@ Color blueColor = Color(0xFFB5EAEA);
 Color purpleColor = Color(0xFFC67ACE);
 Color greenColor = Color(0xFFE8F5E9);
 Color bgColor = Color(0xFFF9F9F9);
+Color whiteColor = Color(0xFFFFFFFF);
 
 TextStyle blackTextStyle = GoogleFonts.nunito(
   color: bkColor,
+);
+TextStyle whiteTextStyle = GoogleFonts.nunito(
+  color: whiteColor,
+  fontWeight: FontWeight.w800,
 );
